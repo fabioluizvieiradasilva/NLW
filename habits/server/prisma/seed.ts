e49bb97748d709c58b4ce45c73e6c443925ff78b
Fabio Luiz Vieira da Silva <fabinvieira@gmail.com>
2023-01-19 11:18:17 -0300

    Primeia Aula

---
+++ habits/server/prisma/seed.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function main() {
+    await prisma.habit.deleteMany()
+    
+    await prisma.habit.create({
+        data:{
+            title:'Fazer 50 flexões',
+            created_at: new Date('2023-01-19T00:00:00.000Z')
+        }
+    })
+
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
